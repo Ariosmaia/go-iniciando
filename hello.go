7b7233acf4be8ab575ce5a6d94032ac532c0f654
Allan <ariosmaia@yahoo.com.br>
2020-04-18 10:58:08 -0300

    If

--- hello.go
+++ hello.go
@@ -12,28 +12,20 @@ func main() {
 	fmt.Println("2 - Exibir Logs")
 	fmt.Println("0 - Sair do Programa")
 
-	// Captura o que variavel digitou
-	// Recebe um modificador e uma variavel
-	// %d representa um inteiro
-	// O que o usuario digitar quero salvar na variavel comando
-	// & indica o edenreço da variavel que quero salvar
 	var comando int
-	//fmt.Scanf("%d", &comando)
-	// Scan não precisa passar o tipo da variavel
 	fmt.Scan(&comando)
 
 	fmt.Println("O comando escolhido foi", comando)
+
+	// Não precisa do abre fecha parenteses
+	// Sempre tem que passar um booleano, ele só aceita expressões
+	if comando == 1 {
+		fmt.Println("Monitorando...")
+	} else if comando == 2 {
+		fmt.Println("Exibindo Logs...")
+	} else if comando == 0 {
+		fmt.Println("Saindo do programa")
+	} else {
+		fmt.Println("Não conheço este comando")
+	}
 }
-
-// O que aprendemos?
-
-// Declaração de variáveis
-// Valor padrão das variáveis sem valor inicial
-// Inferência de tipos de variáveis
-// Descobrir o tipo da variável
-// Através da função TypeOf, do pacote reflect
-// Declaração curta de variáveis
-// Ler dados digitados do usuário
-// Através das funções Scanf e Scan, do pacote fmt
-// Mais convenções do Go
-// Variáveis e imports não utilizados são deletados
