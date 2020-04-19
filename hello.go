87a2201506934d06cdd4155976244d463d3e0832
Allan <ariosmaia@yahoo.com.br>
2020-04-18 21:11:47 -0300

    Formatando tempo em Go

--- hello.go
+++ hello.go
@@ -122,14 +122,13 @@ func leSitesDoArquivo() []string {
 }
 
 func registraLog(site string, status bool) {
-	// os.O_APPEND colocar o item no final da linha
 	arquivo, err := os.OpenFile("log.txt", os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
 
 	if err != nil {
 		fmt.Println(err)
 	}
 
-	arquivo.WriteString(site + " - online: " + strconv.FormatBool(status) + "\n")
+	arquivo.WriteString(time.Now().Format("02/01/2006 15:04:05") + " - " + site + " - online: " + strconv.FormatBool(status) + "\n")
 
 	arquivo.Close()
 }
